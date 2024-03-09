07fc451af7c9e0893b26727d3f789853fa4539a2
Abdul Karim <abdulkarimmandvia76@gmail.com>
2024-03-08 21:59:00 -0800

    chapter2-completed

---
+++ app.mjs
@@ -0,0 +1,37 @@
+//Problem 1
+let username;
+console.log(`UserName: ${username}`) //undefined
+
+// Problem 2
+let myName =`Abdul Karim Mandvia `;
+console.log(`My FullName is: ${myName}`); //My FullName is Abdul Karim Mandvia
+
+// Problem 3
+let tittled ; //initalize variable
+tittled =  `Hello World`; // Assign Value To Variable
+alert(`${tittled}`) //Print Output
+
+//Problem 4 
+//Taking Three Input
+let studentName = prompt(`Enter Your Name:`);
+let age = parseInt(prompt(`Enter Your Age:`));
+let course = prompt(`Enter Your Course:`);
+//Output 
+alert(`Hello ${studentName}`);
+alert(`You are ${age} year old`);
+alert(`Enroll in  ${course} Course`);
+
+//Problem 6 
+let email = prompt("Enter Your Email-Address:");
+alert(`Your Email Address is ${email}`);
+
+//Problem 7
+let book = `A smarter way to learn JavaScript.`;
+alert(`I am trying to learn from the book ${book}`);
+
+//Problem 8 
+document.write("<h1>Yah! I can write HTML content through JavaScript</h1>");
+
+//Problem 9 
+let design = `▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬`;
+alert(`${design}`)
